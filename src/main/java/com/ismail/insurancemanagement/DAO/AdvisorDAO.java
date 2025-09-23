6fbf185713e1f2465e911f6b0c38361b8d724060
ismail baoud <ibaoud46@egmail.com>
2025-09-23 14:05:15 +0100

    add the dao Dao functions

--- src/main/java/com/ismail/insurancemanagement/DAO/AdvisorDAO.java
+++ src/main/java/com/ismail/insurancemanagement/DAO/AdvisorDAO.java
@@ -2,8 +2,10 @@ package main.java.com.ismail.insurancemanagement.DAO;
 
 import main.java.com.ismail.insurancemanagement.config.ConnectionDB;
 import main.java.com.ismail.insurancemanagement.model.Advisor;
+import main.java.com.ismail.insurancemanagement.model.Client;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class AdvisorDAO {
@@ -25,15 +27,13 @@ public class AdvisorDAO {
             stmt.setString(2, advisor.getFirstName());
             stmt.setString(3, advisor.getLastName());
             stmt.setString(4, advisor.getEmail());
-            stmt.executeUpdate();
-            return true;
+            return stmt.executeUpdate() > 0;
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-
-    public boolean delete(UUID id) {
+    public boolean delete(String id) {
         String sql = "DELETE FROM Advisor where id = ?";
         try(PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setObject(1, id);
@@ -44,7 +44,7 @@ public class AdvisorDAO {
         }
     }
 
-    public Advisor findAdvisor(UUID id) {
+    public Advisor findAdvisor(String id) {
         Advisor advisor;
         String sql = "SELECT * FROM Advisor where id = ?";
         try (PreparedStatement stmt = conn.prepareStatement(sql)){
@@ -52,12 +52,33 @@ public class AdvisorDAO {
             ResultSet resultSet = stmt.executeQuery();
             if(resultSet.next()){
                advisor = new Advisor(resultSet.getString("firstname"), resultSet.getString("lastname"),resultSet.getString("email"));
-               advisor.setId((UUID) resultSet.getObject("id"));
+               advisor.setId(UUID.fromString(resultSet.getString("id")));
                return  advisor;
             }
             else {
                 return null;
             }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ArrayList<Client> AdvisorClients(UUID id) {
+        Client client;
+        String sql = "SELECT * FROM Adviser_client where id = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)){
+            ArrayList<Client> clients = new ArrayList<>();
+            stmt.setObject(1, id);
+            ResultSet resultSet = stmt.executeQuery();
+            if(resultSet.next()){
+                client = new Client(resultSet.getString("firstname"), resultSet.getString("lastname"),resultSet.getString("email"));
+                client.setId((UUID) resultSet.getObject("id"));
+                clients.add(client);
+            }
+            else {
+                return null;
+            }
+            return clients;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
--- src/main/java/com/ismail/insurancemanagement/DAO/ClientDAO.java
+++ src/main/java/com/ismail/insurancemanagement/DAO/ClientDAO.java
@@ -1,4 +1,80 @@
 package main.java.com.ismail.insurancemanagement.DAO;
 
+import main.java.com.ismail.insurancemanagement.config.ConnectionDB;
+import main.java.com.ismail.insurancemanagement.model.Advisor;
+import main.java.com.ismail.insurancemanagement.model.Client;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
 public class ClientDAO {
+    Connection conn;
+
+    public ClientDAO() {
+        conn = ConnectionDB.getInstance().getConnection();
+    }
+
+    public boolean create(Client client , String id) {
+        String sql = "INSERT INTO Client (id , firstname , lastname , email , idAdvisor) value (?, ?, ? ,? ,?)";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setObject(1, client.getId());
+            stmt.setString(2, client.getFirstName());
+            stmt.setString(3, client.getLastName());
+            stmt.setString(4, client.getEmail());
+            stmt.setObject(5, id);
+            return stmt.executeUpdate() > 0;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Client findClientById(String id) {
+        Client client;
+        Advisor advisor;
+        String sql = "Select c.* , a.* from Client c join Advisor a ON c.idAdvisor = a.id where c.id = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                client = new Client(rs.getString("c.firstname"), rs.getString("c.lastname"), rs.getString("c.email"));
+                client.setId(UUID.fromString(rs.getString("c.id")));
+                advisor = new Advisor(rs.getString("a.firstname"),rs.getString("a.lastname"),rs.getString("a.email"));
+                advisor.setId(UUID.fromString(rs.getString("a.id")));
+                client.setAdvisor(advisor);
+                return client;
+            }else {
+                return null;
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Client findClientByLastName(String lastName) {
+        Client client;
+        String sql = "SELECT c.* a.* from Client joint Advisor on c.lastname = a.lastname where c.lastname = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, lastName);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                client = new Client(
+                        rs.getString("c.firstname"), rs.getString("c.lastname"),rs.getString("c.email")
+                );
+                client.setId(UUID.fromString(rs.getString("c.id")));
+                Advisor advisor = new Advisor(
+                        rs.getString("a.firstname"), rs.getString("a.lastname") , rs.getString("a.email")
+                );
+                advisor.setId(UUID.fromString(rs.getString("a.id")));
+                client.setAdvisor(advisor);
+                return client;
+            }else {
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
